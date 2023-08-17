57df391ec2efc0bcb4d2b25c36f2f451356335c6
Adonai Dominguez <adonaidominguezes@gmail.com>
2023-08-17 20:44:13 +0200

    Initial Commit

---
+++ Pokedex-Masters/Pokedex_MastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_MastersApp.swift
+//  Pokedex-Masters
+//
+//  Created by Doni on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_MastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
