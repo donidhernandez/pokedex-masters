febd8e0f0984007dacc616d46e07fe770e4d6913
Adonai Dominguez <adonaidominguezes@gmail.com>
2023-09-09 11:48:12 +0200

    create model, endpoint, and generic networkmanager

--- Pokedex-Masters/Base/API/Endpoint.swift
+++ Pokedex-Masters/Base/API/Endpoint.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+enum Endpoint {
+    case allPokemon(page: Int)
+    case pokemonDetails(name: String)
+}
+
+extension Endpoint {
+    var host: String { "pokeapi.co" }
+    var basePath: String { "api/v2" }
+    
+    var path: String {
+        switch self  {
+            case .allPokemon:
+                return "\(basePath)/pokemon"
+            case .pokemonDetails(let name):
+                return "\(basePath)/pokemon/\(name)"
+        }
+    }
+    
+    var queryItems: [String: String]? {
+        switch self {
+            case .allPokemon(let page):
+                return ["page": "\(page)"]
+            default:
+                return nil
+        }
+    }
+    
+    var url: URL? {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = host
+        urlComponents.path = path
+        
+        var requestQueryItems = [URLQueryItem]()
+        
+        queryItems?.forEach({ item in
+            requestQueryItems.append(URLQueryItem(name: item.key, value: item.value))
+        })
+        
+        urlComponents.queryItems = requestQueryItems
+        
+        return urlComponents.url
+    }
+}
--- Pokedex-Masters/Base/API/NetworkManager.swift
+++ Pokedex-Masters/Base/API/NetworkManager.swift
@@ -6,3 +6,76 @@
 //
 
 import Foundation
+
+protocol NetworkManagerProtocol {
+    func request<T: Codable>(session: URLSession, _ endpoint: Endpoint, type: T.Type) async throws -> T
+}
+
+class NetworkManager: NetworkManagerProtocol {
+    
+    static let shared = NetworkManager()
+    
+    private init () {}
+    
+    func request<T>(session: URLSession, _ endpoint: Endpoint, type: T.Type ) async throws -> T where T : Decodable, T : Encodable {
+        
+        guard let url = endpoint.url else {
+            throw NetworkError.invalidURL
+        }
+        
+        let urlRequest = URLRequest(url: url)
+        
+        let (data, response) = try await session.data(for: urlRequest)
+        
+        guard let httpResponse = response as? HTTPURLResponse, (200...300) ~= httpResponse.statusCode else {
+            let statusCode = (response as! HTTPURLResponse).statusCode
+            throw NetworkError.invalidStatusCode(statusCode: statusCode)
+        }
+        
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        do {
+            return try jsonDecoder.decode(T.self, from: data)
+        } catch {
+            throw NetworkError.failedToDecode(error: error)
+        }
+    }
+    
+}
+
+extension NetworkManager {
+    enum NetworkError: LocalizedError {
+        case invalidURL
+        case invalidStatusCode(statusCode: Int)
+        case failedToDecode(error: Error)
+    }
+}
+
+extension NetworkManager.NetworkError: Equatable {
+    static func == (lhs: NetworkManager.NetworkError, rhs: NetworkManager.NetworkError) -> Bool {
+        switch(lhs, rhs) {
+            case (.invalidURL, .invalidURL):
+                return true
+            case(.failedToDecode(let lhsType), .failedToDecode(let rhsType)):
+                return lhsType.localizedDescription == rhsType.localizedDescription
+            case(.invalidStatusCode(let lhsType), .invalidStatusCode(let rhsType)):
+                return lhsType == rhsType
+            default:
+                return false
+        }
+    }
+}
+
+extension NetworkManager.NetworkError {
+    var errorDescription: String? {
+        switch self {
+            case .invalidURL:
+                return "URL isn't valid"
+            case .invalidStatusCode:
+                return "Status code falls into the wrong range"
+            case .failedToDecode:
+                return "Failed to decode"
+        }
+    }
+}
