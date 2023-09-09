3263addf3274569429d8058bb45acb5ded345b5a
Adonai Dominguez <adonaidominguezes@gmail.com>
2023-09-09 11:47:59 +0200

    create model, endpoint, and generic networkmanager

---
+++ Pokedex-Masters/Base/API/Endpoint.swift
@@ -0,0 +1,8 @@
+//
+//  Endpoint.swift
+//  Pokedex-Masters
+//
+//  Created by Doni on 9/9/23.
+//
+
+import Foundation
---
+++ Pokedex-Masters/Base/API/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  Pokedex-Masters
+//
+//  Created by Doni on 9/9/23.
+//
+
+import Foundation
